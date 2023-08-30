8e69dcee2f541953babec28a9df856abe91de2c8
xF3rd <102755227+xF3rd@users.noreply.github.com>
2023-08-30 13:14:21 -0300

    Add files via upload

---
+++ Lab3/Ex1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct produto{
+
+    int cod,quant;
+    float valCompra,valVenda;
+
+}prod;
+
+
+prod* atribui(int a, int b, float c, float d){
+    prod *aux;
+
+    aux = (prod *)malloc(1*sizeof(prod));
+    aux->cod=a;
+    aux->quant=b;
+    aux->valCompra=c;
+    aux->valVenda=d;
+
+    return aux;
+
+}
+
+int main() {
+    prod *produto[10];
+    int a,b;
+    float c,d;
+
+    for(int i = 0 ; i < 2 ; i++){
+        printf("Codigo do prod:");
+        scanf("%d",&a);
+
+        printf("Codigo do quant:");
+        scanf("%d",&b);
+
+        printf("Codigo do compra:");
+        scanf("%f",&c);
+
+        printf("Codigo do venda:");
+        scanf("%f",&d);
+
+        produto[i] = atribui(a,b,c,d);
+
+    }
+
+    for(int i=0; i<2 ; i++){
+        printf("\n %d \n %d \n %.2f \n %.2f",produto[i]->cod,produto[i]->quant,produto[i]->valCompra,produto[i]->valVenda);
+    }
+
+    free(produto[10]);
+
+
+return 0;
+}
